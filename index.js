2bc948960d1053968cc4a26bdfdba29e73f9a2d2
Francisco0102-code <francisco.araujo927@aluno.ce.gov.br>
2025-09-17 10:01:39 -0300

    site da vtec

--- index.js
+++ index.js
@@ -1,111 +1,82 @@
-    const menuToggle = document.querySelector('.menu-toggle');
-    const menu = document.querySelector('.menu');
-    const links = document.querySelectorAll('.menu a');
+// MENU MOBILE
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+const links = document.querySelectorAll('.menu a');
 
-    menuToggle.addEventListener('click', () => {
-      menu.classList.toggle('ativo');
-    });
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('ativo');
+});
 
-    links.forEach(link => {
-      link.addEventListener('click', () => {
-        menu.classList.remove('ativo');
-      });
-    });
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('ativo');
+  });
+});
 
+// CARROSSEL DE FOTOS
 !function(d){
-    let itemClassName = "carrosel-foto",
-    items = d.getElementsByClassName(itemClassName),
-    totalItems = items.length,
-    slide = 0,
+  let itemClassName = "carrosel-foto",
+  items = d.getElementsByClassName(itemClassName),
+  totalItems = items.length,
+  slide = 0,
+  moving = true;
+
+  function setInitialClasses(){
+    // marca último como prev, primeiro como active e segundo como next
+    items[totalItems - 1].classList.add("prev");
+    items[0].classList.add("active");
+    items[1].classList.add("next");
+  }
+
+  function setEventListeners(){
+    let next = d.getElementsByClassName("carrosel-button--next")[0];
+    let prev = d.getElementsByClassName("carrosel-button--prev")[0];
+    next.addEventListener("click", moveNext);
+    prev.addEventListener("click", movePrev);
+  }
+
+  function disableInteraction(){
     moving = true;
+    setTimeout(()=>{moving=false},500);
+  }
 
-    function setInitialClasses(){
-        items[totalItems - 1].classList.add("prev");
-        items[0].classList.add("active")
-        items[1].classList.add("next");
+  function moveNext(){
+    if(!moving){
+      if(slide === (totalItems - 1)) slide=0; else slide++;
+      moveCarroselPara(slide);
     }
+  }
 
-    function setEventListeners(){
-        let next = d.getElementsByClassName("carrosel-button--next")[0];
-        let prev = d.getElementsByClassName("carrosel-button--prev")[0];
-
-        next.addEventListener("click", moveNext);
-        prev.addEventListener("click", movePrev);
+  function movePrev(){
+    if(!moving){
+      if(slide === 0) slide=(totalItems - 1); else slide--;
+      moveCarroselPara(slide);
     }
+  }
 
-    function moveNext(){
-        console.log("funcionando")
-        if(!moving){
-            if(slide === (totalItems - 1)){
-                slide = 0
-            } else {
-                slide++;
-            }
+  function moveCarroselPara(slide){
+    if(!moving){
+      disableInteraction();
+      let newPrev=slide-1, newNext=slide+1, oldPrev=slide-2, oldNext=slide+2;
 
-            moveCarroselPara(slide)
-        }
+      if(newPrev<0)newPrev=totalItems-1;
+      if(newNext>=totalItems)newNext=0;
+      if(oldPrev<0)oldPrev=totalItems+oldPrev;
+      if(oldNext>=totalItems)oldNext=oldNext-totalItems;
+
+      items[oldPrev].className=itemClassName;
+      items[oldNext].className=itemClassName;
+      items[newPrev].className=itemClassName+" prev";
+      items[slide].className=itemClassName+" active";
+      items[newNext].className=itemClassName+" next";
     }
+  }
 
-    function movePrev(){
-        console.log("funcionando prev")
-        if(!moving){
-            if(slide === 0){
-                slide = (totalItems - 1);
-            } else {
-                slide--;
-            }
-        }
+  function initCarrosel(){
+    setInitialClasses();
+    setEventListeners();
+    moving=false;
+  }
 
-        moveCarroselPara(slide);
-    }
-
-    function disableInteraction(){
-        moving = true;
-
-        setTimeout(() => {
-            moving = false;
-        }, 500)
-    }
-
-    function moveCarroselPara(slide){
-        if(!moving){
-            disableInteraction();
-
-            let newPrev = slide - 1, newNext = slide + 1, oldPrev = slide - 2, oldNext = slide + 2;
-
-            if((totalItems - 1) > 3){
-                if(newPrev <= 0){
-                    oldPrev = (totalItems - 1);
-                } else if(newNext >= (totalItems - 1)){
-                    oldNext = 0;
-                }
-
-                if(slide === 0){
-                    newPrev = (totalItems - 1);
-                    oldPrev = (totalItems - 2);
-                    oldNext = (slide + 1);
-                } else if(slide === (totalItems - 1)){
-                    newPrev = (slide - 1);
-                    newNext = 0;
-                    oldNext = 1;
-                }
-
-                items[oldPrev].className = itemClassName;
-                items[oldNext].className = itemClassName;
-
-                items[newPrev].className = itemClassName + " prev";
-                items[slide].className = itemClassName + " active"
-                items[newNext].className = itemClassName + " next";
-            }
-        }
-    }
-
-    function initCarrosel(){
-        setInitialClasses();
-        setEventListeners();
-
-        moving = false;
-    }
-
-    initCarrosel();
-}(document)
+  initCarrosel();
+}(document);
